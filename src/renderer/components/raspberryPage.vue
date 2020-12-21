e9e388b583d26e74be782c3a59658e7b75dac909
yaodongcai <yaodongcai@qq.com>
2020-12-21 08:49:07 +0800

    【修复代码】【dcyao】增加了usb 读取的功能

--- src/renderer/components/raspberryPage.vue
+++ src/renderer/components/raspberryPage.vue
@@ -92,26 +92,38 @@
                 </p>
                 <p class="raspberry-btn"><el-button @click="handleClick('photo')" size="mini" type="primary">
                     手动拍照</el-button></p>
-                <p class="raspberry-btn">
-                    <el-button size="mini" type="primary" @click="handleClick('downloadStart')">
-                        下载开始
-                    </el-button>
-                </p>
-                <p class="raspberry-btn">
-                    <el-button size="mini" type="primary" @click="handleClick('downloadEnd')">
-                        下载结束
-                    </el-button>
-                </p>
-                <p class="raspberry-btn">
-                    <el-button size="mini" type="primary" @click="handleClick('audioStart')">
-                        录像开始
-                    </el-button>
-                </p>
-                <p class="raspberry-btn">
-                    <el-button size="mini" type="primary" @click="handleClick('audioEnd')">
-                        录像结束
-                    </el-button>
-                </p>
+                <p class="raspberry-btn"><el-button @click="handleDownloadStartClick()" size="mini" type="primary">
+                   下载开始</el-button></p>
+                <!--<p class="raspberry-btn"><el-button @click="handle2Click('SDToggleOn')" size="mini" type="primary">-->
+                    <!--SD卡通电</el-button></p>-->
+                <!--<p class="raspberry-btn"><el-button @click="handle2Click('SDOn')" size="mini" type="primary">-->
+                    <!--USB通电</el-button></p>-->
+                <p class="raspberry-btn"><el-button @click="handleDownloadEndClick()" size="mini" type="primary">
+                    下载结束</el-button></p>
+                <!--<p class="raspberry-btn"><el-button @click="handle2Click('SDToggleOff')" size="mini" type="primary">-->
+                    <!--SD卡Off</el-button></p>-->
+                <!--<p class="raspberry-btn"><el-button @click="handle2Click('SDOff')" size="mini" type="primary">-->
+                    <!--USB断点</el-button></p>-->
+                <!--<p class="raspberry-btn">-->
+                    <!--<el-button size="mini" type="primary" @click="handleClick('downloadStart')">-->
+                        <!--下载开始-->
+                    <!--</el-button>-->
+                <!--</p>-->
+                <!--<p class="raspberry-btn">-->
+                    <!--<el-button size="mini" type="primary" @click="handleClick('downloadEnd')">-->
+                        <!--下载结束-->
+                    <!--</el-button>-->
+                <!--</p>-->
+                <!--<p class="raspberry-btn">-->
+                    <!--<el-button size="mini" type="primary" @click="handleClick('audioStart')">-->
+                        <!--录像开始-->
+                    <!--</el-button>-->
+                <!--</p>-->
+                <!--<p class="raspberry-btn">-->
+                    <!--<el-button size="mini" type="primary" @click="handleClick('audioEnd')">-->
+                        <!--录像结束-->
+                    <!--</el-button>-->
+                <!--</p>-->
             </el-col>
         </el-row>
         <el-dialog title="IP设置" :visible.sync="dialogVisible">
@@ -177,6 +189,10 @@
         timeHandle: null, // 设置定时的句柄
         url: 'http://192.168.0.10:7001/respberry', // 192.168.0.10 192.168.43.131
         commandCodeObj: {
+          SDToggleOn: 'SDToggleOn',
+          SDToggleOff: 'SDToggleOff',
+          SDOn: 'SDOn',
+          SDOff: 'SDOff',
           on: 'on', // 开机命令
           off: 'off', // 关机命令
           photo: 'photo', // 手动拍照
@@ -294,7 +310,7 @@
         if (!this.isSetTime) {
           // 如果是勾选了 取消定时器
           this.$http
-            .post(this.url + '/writePortIsIntertime', {
+            .post(this.url + '/GPIOControllerIntertime', {
               send: 'noPhoto'
             })
             .then((res) => {
@@ -315,7 +331,7 @@
           }
           // 发送对应的指令
           this.$http
-            .post(this.url + '/writePortIsIntertime', {
+            .post(this.url + '/GPIOControllerIntertime', {
               send: 'photo',
               timeOut: time
             })
@@ -354,7 +370,7 @@
               const obj = res.data.list
               const addressList = (obj.address + '').split('.')
               addressList.map((item, index) => {
-                console.log('this.lists[0][index]',this.lists[0].list)
+                console.log('this.lists[0][index]', this.lists[0].list)
                 this.lists[0].list[index].ip = item
               })
               const netmaskList = (obj.netmask + '').split('.')
@@ -393,12 +409,71 @@
             console.log('res', res)
           })
       },
+      handleDownloadEndClick () {
+        this.handle2Click('SDOff')
+        // 300ms 后开始再次调用新的参数
+        setTimeout(() => {
+          this.handle2Click('SDToggleOff')
+          // 这个时候再调用一个接口来挂载当前的数据即可
+          this.$http
+            .get(this.url + '/downLoadEnd').then(res => {
+            })
+        }, 300)
+      },
+      handleDownloadStartClick () {
+        this.handle2Click('SDToggleOn')
+        // 300ms 后开始再次调用新的参数
+        setTimeout(() => {
+          this.handle2Click('SDOn')
+          // 这个时候再调用一个接口来挂载当前的数据即可
+          setTimeout(() => {
+            // 需要调用download 接口
+            this.$http
+              .get(this.url + '/downLoadStart').then(res => {
+                setTimeout(() => {
+                  child.exec('explorer.exe "ftp://pi:raspberry@192.168.0.10"', function (err, sto) {
+                    console.log('err', err, sto)
+                  })
+                }, 1000)
+              })
+          }, 3000) // 需要延时一秒来打开
+        }, 1000)
+      },
+      handle2Click (type) {
+        let message = ''
+        message = this.commandCodeObj[type + '']
+        console.log('message', message, 'type', type)
+        this.$http
+          .post(this.url + '/GPIOControllerByGPIO', {
+            send: message
+          })
+          .then((res) => {
+            console.log('res', res)
+            if (type === 'downloadStart') {
+              setTimeout(() => {
+                child.exec('explorer.exe "ftp://pi:raspberry@192.168.0.10"', function (err, sto) {
+                  console.log('err', err, sto)
+                })
+              }, 1000) // 需要延时一秒来打开
+              // 这个时候需要判断这个res 的内容
+              if (res.status === 2) {
+                // 表示这个其实是一个错误的信息
+                this.$message.error(res.message + '')
+              } else {
+                this.$message.success('发送成功')
+              }
+              return
+            }
+
+            this.$message.success('发送成功')
+          })
+      },
       handleClick (type) {
         let message = ''
         message = this.commandCodeObj[type + '']
         console.log('message', message, 'type', type)
         this.$http
-          .post(this.url + '/writePort', {
+          .post(this.url + '/GPIOController', {
             send: message
           })
           .then((res) => {
@@ -431,23 +506,10 @@
           background: 'rgba(0, 0, 0, 0.7)'
         })
         // 随便发一个请求去请求树莓派地址 然后看有没有响应即可
-        this.$http.get(this.url + '/serialPortList').then(res => {
+        this.$http.get(this.url + '/initGPIOController').then(res => {
           // 获取了所有的数据
           loading.close()
-          this.$message.success('连接成功，初始化接口数据')
-          const list = res.data.list.filter(item => {
-            return !!item.hasOwnProperty('path')
-          })
-          // 当前的接口就是为list[0].path
-          const portName = list[0].path
-          portName &&
-          this.$http
-            .post(this.url + '/openPortByName', {
-              portName: portName
-            })
-            .then((res) => {
-              this.$message.success('USB端口打开成功，开始读写数据')
-            })
+          this.$message.success('初始化各个GPIO口成功')
         // 开始初始化usb的接口 开始提交数据
         }).catch(() => {
           loading.close()
