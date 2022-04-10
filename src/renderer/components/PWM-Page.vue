5c6aaccb7bcfa76430e4df40d747306df31265ca
yaodongcai <yaodongcai@qq.com>
2022-04-10 15:29:20 +0800

    【修复代码】【dcyao】 增加了多个PWM的控制机子 修改了标题的部分

--- src/renderer/components/PWM-Page.vue
+++ src/renderer/components/PWM-Page.vue
@@ -1,68 +1,360 @@
 <template>
     <div class="tab">
-        <el-row>
-            <el-col :span="24">
-                <p>PWM1----PIN12----GPIO18</p>
-                <p>当前的占空比为: {{ pwm0 }}</p>
+        <div class="pwm-name">
+            电机A控制口 对应引脚为Pin 12(Gpio 18)
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                正转
+            </div>
+            <div class="pwm-item-right">
                 <el-slider
                         show-tooltip
                         show-stops
-                        :step="0.01"
-                        v-model="pwm0"
-                        :max="2.5"
-                        :min="0.8"
+                        :step="0.005"
+                        v-model="pwm0.left"
+                        :max="1.5"
+                        :min="0.81"
                         width="100%"
-                        @change="change"
-                >
-                </el-slider>
-            </el-col>
-            <!--<el-col :span="6">-->
-                <!--<p>PWM1&#45;&#45;&#45;&#45;PIN13&#45;&#45;&#45;&#45;GPIO13</p>-->
-                <!--<el-slider show-tooltip v-model="pwm1" vertical height="200px">-->
-                <!--</el-slider>-->
-            <!--</el-col>-->
-            <!--<el-col :span="6">-->
-                <!--<p>PWM1&#45;&#45;&#45;&#45;PIN14&#45;&#45;&#45;&#45;GPIO14</p>-->
-                <!--<el-slider show-tooltip v-model="pwm2" vertical height="200px">-->
-                <!--</el-slider>-->
-            <!--</el-col>-->
-            <!--<el-col :span="6">-->
-                <!--<p>PWM1&#45;&#45;&#45;&#45;PIN15&#45;&#45;&#45;&#45;GPIO16</p>-->
-                <!--<el-slider show-tooltip v-model="pwm3" vertical height="200px">-->
-                <!--</el-slider>-->
-            <!--</el-col>-->
-        </el-row>
+                        @change="(val) => change(val, 18)"
+                />
+            </div>
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                正转的占空比为:{{ pwm0.left }}  反转的占空比为:{{ pwm0.right}}
+            </div>
+            <div class="pwm-item-right">
+                <el-button size="mini" type="danger" @click="change(1.5, 18)">停止</el-button>
+            </div>
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                反转
+            </div>
+            <div class="pwm-item-right">
+                <el-slider
+                        show-tooltip
+                        show-stops
+                        :step="0.005"
+                        v-model="pwm0.right"
+                        :max="2.4"
+                        :min="1.5"
+                        width="100%"
+                        @change="(val) => change(val, 18)"
+                />
+            </div>
+        </div>
+
+        <div class="pwm-name">
+            电机B控制口 对应引脚为Pin 32(Gpio 12)
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                正转
+            </div>
+            <div class="pwm-item-right">
+                <el-slider
+                        show-tooltip
+                        show-stops
+                        :step="0.005"
+                        v-model="pwm1.left"
+                        :max="1.5"
+                        :min="0.81"
+                        width="100%"
+                        @change="(val) => change(val, 12)"
+                />
+            </div>
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                正转的占空比为:{{ pwm1.left }}  反转的占空比为:{{ pwm1.right}}
+            </div>
+            <div class="pwm-item-right">
+                <el-button size="mini" type="danger" @click="change(1.5, 12)">停止</el-button>
+            </div>
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                反转
+            </div>
+            <div class="pwm-item-right">
+                <el-slider
+                        show-tooltip
+                        show-stops
+                        :step="0.005"
+                        v-model="pwm1.right"
+                        :max="2.4"
+                        :min="1.5"
+                        width="100%"
+                        @change="(val) => change(val, 12)"
+                />
+            </div>
+        </div>
+
+        <div class="pwm-name">
+            电机C控制口 对应引脚为Pin 33(Gpio 13)
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                正转
+            </div>
+            <div class="pwm-item-right">
+                <el-slider
+                        show-tooltip
+                        show-stops
+                        :step="0.005"
+                        v-model="pwm2.left"
+                        :max="1.5"
+                        :min="0.81"
+                        width="100%"
+                        @change="(val) => change(val, 13)"
+                />
+            </div>
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                正转的占空比为:{{ pwm2.left }}  反转的占空比为:{{ pwm2.right}}
+            </div>
+            <div class="pwm-item-right">
+                <el-button size="mini" type="danger" @click="change(1.5, 13)">停止</el-button>
+            </div>
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                反转
+            </div>
+            <div class="pwm-item-right">
+                <el-slider
+                        show-tooltip
+                        show-stops
+                        :step="0.005"
+                        v-model="pwm2.right"
+                        :max="2.4"
+                        :min="1.5"
+                        width="100%"
+                        @change="(val) => change(val, 13)"
+                />
+            </div>
+        </div>
+
+
+        <div class="pwm-name">
+            电机E控制口 对应引脚为Pin 35(Gpio 19)
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                正转
+            </div>
+            <div class="pwm-item-right">
+                <el-slider
+                        show-tooltip
+                        show-stops
+                        :step="0.005"
+                        v-model="pwm3.left"
+                        :max="1.5"
+                        :min="0.81"
+                        width="100%"
+                        @change="(val) => change(val, 19)"
+                />
+            </div>
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                正转的占空比为:{{ pwm3.left }}  反转的占空比为:{{ pwm3.right}}
+            </div>
+            <div class="pwm-item-right">
+                <el-button size="mini" type="danger" @click="change(1.5, 19)">停止</el-button>
+            </div>
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                反转
+            </div>
+            <div class="pwm-item-right">
+                <el-slider
+                        show-tooltip
+                        show-stops
+                        :step="0.005"
+                        v-model="pwm3.right"
+                        :max="2.4"
+                        :min="1.5"
+                        width="100%"
+                        @change="(val) => change(val, 19)"
+                />
+            </div>
+        </div>
+
+        <div class="pwm-name">
+            电机D控制口 对应引脚为Pin 38(Gpio 20)
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                正转
+            </div>
+            <div class="pwm-item-right">
+                <el-slider
+                        show-tooltip
+                        show-stops
+                        :step="0.005"
+                        v-model="pwm4.left"
+                        :max="1.5"
+                        :min="0.81"
+                        width="100%"
+                        @change="(val) => change(val, 20)"
+                />
+            </div>
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                正转的占空比为:{{ pwm4.left }}  反转的占空比为:{{ pwm4.right}}
+            </div>
+            <div class="pwm-item-right">
+                <el-button size="mini" type="danger" @click="change(1.5, 20)">停止</el-button>
+            </div>
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                反转
+            </div>
+            <div class="pwm-item-right">
+                <el-slider
+                        show-tooltip
+                        show-stops
+                        :step="0.005"
+                        v-model="pwm4.right"
+                        :max="2.4"
+                        :min="1.5"
+                        width="100%"
+                        @change="(val) => change(val, 20)"
+                />
+            </div>
+        </div>
+
+
+        <div class="pwm-name">
+            电机F控制口 对应引脚为Pin 37(Gpio 26)
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                正转
+            </div>
+            <div class="pwm-item-right">
+                <el-slider
+                        show-tooltip
+                        show-stops
+                        :step="0.005"
+                        v-model="pwm5.left"
+                        :max="1.5"
+                        :min="0.81"
+                        width="100%"
+                        @change="(val) => change(val, 26)"
+                />
+            </div>
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                正转的占空比为:{{ pwm5.left }}  反转的占空比为:{{ pwm5.right}}
+            </div>
+            <div class="pwm-item-right">
+                <el-button size="mini" type="danger" @click="change(1.5, 26)">停止</el-button>
+            </div>
+        </div>
+        <div class="pwm-item">
+            <div class="pwm-item-left">
+                反转
+            </div>
+            <div class="pwm-item-right">
+                <el-slider
+                        show-tooltip
+                        show-stops
+                        :step="0.005"
+                        v-model="pwm5.right"
+                        :max="2.4"
+                        :min="1.5"
+                        width="100%"
+                        @change="(val) => change(val, 26)"
+                />
+            </div>
+        </div>
     </div>
 </template>
+<style lang="scss">
+    .tab {
+        margin: 20px;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
 
+        .pwm-name {
+            color: #669900;
+            min-height: 30px;
+        }
+        .pwm-item {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            min-height: 25px;
+            .pwm-item-left {
+                margin-left: 10px;
+                margin-right: 10px;
+            }
+            .pwm-item-right {
+                flex: 1
+            }
+        }
+    }
+</style>
 <script>
   export default {
     name: 'LandingPage',
     data () {
       return {
         url: 'http://192.168.0.10:7001/respberry',
-        pwm0: 1.5,
-        pwm1: 0,
-        pwm2: 0,
-        pwm3: 0
+        pwm0: {
+          left: 1.5,
+          right: 1.5
+        },
+        pwm1: {
+          left: 1.5,
+          right: 1.5
+        },
+        pwm2: {
+          left: 1.5,
+          right: 1.5
+        },
+        pwm3: {
+          left: 1.5,
+          right: 1.5
+        },
+        pwm4: {
+          left: 1.5,
+          right: 1.5
+        },
+        pwm5: {
+          left: 1.5,
+          right: 1.5
+        }
       }
     },
     created () {},
     methods: {
-      change (value) {
-        console.log('value', value)
+      change (value, port) {
+        console.log('value', value, port)
         // 延迟一秒钟开始发送即可 加入延迟函数即可
         let handleTimeOut = setTimeout(() => {
           // 如果value 是
-          const num = parseFloat(value) * 100
-          this.setPWMDutyTime(num)
+          const num = parseFloat(value) * 200
+          console.log('num', num, 'port', port)
+          this.setPWMDutyTime(num, port)
           clearTimeout(handleTimeOut)
           // 延迟200ms 即可
         }, 200)
       },
-      setPWMDutyTime (count) {
+      setPWMDutyTime (count, port) {
         this.$http
           .post(this.url + '/pwm', {
+            port: port,
             count: count
           })
           .then((res) => {
@@ -74,10 +366,5 @@
   }
 </script>
 
-<style lang="scss">
-    .tab {
-        margin: 20px;
-        width: 100%;
-    }
-</style>
+
 
