264cf54eb34a61f595ff13364fd7efc705b19c95
yaodongcai <yaodongcai@qq.com>
2022-04-10 11:12:12 +0800

    【修复代码】【dcyao】 修复了速度的问题

--- src/renderer/components/PWM-Page.vue
+++ src/renderer/components/PWM-Page.vue
@@ -1,35 +1,36 @@
 <template>
     <div class="tab">
         <el-row>
-            <el-col :span="6">
+            <el-col :span="24">
                 <p>PWM1----PIN12----GPIO18</p>
-                <p>当前的占空比为: {{ formatTooltip(pwm0) }}</p>
+                <p>当前的占空比为: {{ pwm0 }}</p>
                 <el-slider
                         show-tooltip
-                        :format-tooltip="formatTooltip"
+                        show-stops
+                        :step="0.01"
                         v-model="pwm0"
-                        vertical
-                        :max="25"
-                        height="200px"
+                        :max="2.5"
+                        :min="0.8"
+                        width="100%"
                         @change="change"
                 >
                 </el-slider>
             </el-col>
-            <el-col :span="6">
-                <p>PWM1----PIN13----GPIO13</p>
-                <el-slider show-tooltip v-model="pwm1" vertical height="200px">
-                </el-slider>
-            </el-col>
-            <el-col :span="6">
-                <p>PWM1----PIN14----GPIO14</p>
-                <el-slider show-tooltip v-model="pwm2" vertical height="200px">
-                </el-slider>
-            </el-col>
-            <el-col :span="6">
-                <p>PWM1----PIN15----GPIO16</p>
-                <el-slider show-tooltip v-model="pwm3" vertical height="200px">
-                </el-slider>
-            </el-col>
+            <!--<el-col :span="6">-->
+                <!--<p>PWM1&#45;&#45;&#45;&#45;PIN13&#45;&#45;&#45;&#45;GPIO13</p>-->
+                <!--<el-slider show-tooltip v-model="pwm1" vertical height="200px">-->
+                <!--</el-slider>-->
+            <!--</el-col>-->
+            <!--<el-col :span="6">-->
+                <!--<p>PWM1&#45;&#45;&#45;&#45;PIN14&#45;&#45;&#45;&#45;GPIO14</p>-->
+                <!--<el-slider show-tooltip v-model="pwm2" vertical height="200px">-->
+                <!--</el-slider>-->
+            <!--</el-col>-->
+            <!--<el-col :span="6">-->
+                <!--<p>PWM1&#45;&#45;&#45;&#45;PIN15&#45;&#45;&#45;&#45;GPIO16</p>-->
+                <!--<el-slider show-tooltip v-model="pwm3" vertical height="200px">-->
+                <!--</el-slider>-->
+            <!--</el-col>-->
         </el-row>
     </div>
 </template>
@@ -40,7 +41,7 @@
     data () {
       return {
         url: 'http://192.168.0.10:7001/respberry',
-        pwm0: 0,
+        pwm0: 1.5,
         pwm1: 0,
         pwm2: 0,
         pwm3: 0
@@ -49,15 +50,20 @@
     created () {},
     methods: {
       change (value) {
+        console.log('value', value)
         // 延迟一秒钟开始发送即可 加入延迟函数即可
+        let handleTimeOut = setTimeout(() => {
+          // 如果value 是
+          const num = parseFloat(value) * 100
+          this.setPWMDutyTime(num)
+          clearTimeout(handleTimeOut)
+          // 延迟200ms 即可
+        }, 200)
       },
-      formatTooltip (val) {
-        return val / 10
-      },
-      setPWMDutyTime () {
+      setPWMDutyTime (count) {
         this.$http
           .post(this.url + '/pwm', {
-            count: 1.5
+            count: count
           })
           .then((res) => {
             const data = res.data.data
@@ -71,6 +77,7 @@
 <style lang="scss">
     .tab {
         margin: 20px;
+        width: 100%;
     }
 </style>
 
